910e6e8270a6e0ca12198a9ce12fbd6e4566726d
zohaib <zohaibshafiq2005@gmail.com>
2024-11-15 01:22:20 +0500

    create a get posts route

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  // GET localhost:3000/posts/:userId
+
+  @Get('/:userId?')
+  public getPosts(@Param('userId') userId: string) {
+    return this.postsService.findAll(userId);
+  }
 }
