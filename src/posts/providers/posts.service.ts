cd1bd067e4b6c2f26e042d82a0001998a354ba69
zohaib <zohaibshafiq2005@gmail.com>
2024-11-18 20:37:08 +0500

    use users service inside posts service

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    // Injecting users service
+    private readonly usersService: UsersService,
+  ) {}
   public findAll(userId: string) {
-    console.log(userId);
+    const user = this.usersService.findOneById(userId);
     // Users Service
     // Find A User
 
     return [
       {
+        user: user,
         title: 'Test Tile',
         content: 'Test Content',
       },
       {
+        user: user,
         title: 'Test Tile 2',
         content: 'Test Content 2',
       },
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { UsersService } from './providers/users.service'; 
+import { UsersService } from './providers/users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
